95332f371f9002950d7082ae1d03aad01192aa8d
רז לוי <bos901@gmail.com>
2020-04-12 21:14:05 +0300

    Updates

---
+++ precache-manifest.f54d51af56f6050a7ffed4ef3bcba618.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955efa520caf411cc7d1d626943507a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bf532c941b8d8cdb6f5",
+    "url": "/static/css/main.983fd447.chunk.css"
+  },
+  {
+    "revision": "e0567652388eb43502bf",
+    "url": "/static/js/2.fea24613.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.fea24613.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4bf532c941b8d8cdb6f5",
+    "url": "/static/js/main.0795a82e.chunk.js"
+  },
+  {
+    "revision": "c0d7c58e510620f2f64c",
+    "url": "/static/js/runtime-main.7b61e083.js"
+  },
+  {
+    "revision": "d7292aa6fa9954c4dfbec679b0f0476a",
+    "url": "/static/media/background.d7292aa6.png"
+  },
+  {
+    "revision": "10d025fd37da390b89cfec0ef23b4acd",
+    "url": "/static/media/profileImg.10d025fd.png"
+  }
+]);
